cc7bb07c59102603de2b48df302af6b81584dcb6
INO <musicbyino@gmail.com>
2026-06-02 16:42:46 -0400

    video.js  AUDIO SECTION OVERHAUL! V1.3

---
+++ videos.js
@@ -0,0 +1,65 @@
+const videos = [
+  {
+    title: "343",
+    featured: true,
+
+    video:
+      "https://pub-9db467ada9744f04b4c0eaf2e4b1ca71.r2.dev/video/343_CMRCrypt_MV.mp4",
+
+    poster:
+      "https://pub-9db467ada9744f04b4c0eaf2e4b1ca71.r2.dev/video/poster/343.jpg",
+
+    date: "4_24_26",
+
+    description:
+      "343 Music Video. Directed/Edited by INO. CO Edited by Eric McFaline. Shot by Renee Spencer. Assisted by Tony Lopez"
+  },
+
+  {
+    title: "flesh",
+    featured: false,
+
+    video:
+      "https://pub-9db467ada9744f04b4c0eaf2e4b1ca71.r2.dev/video/flesh_CMRCrypt_MV.mp4",
+
+    poster:
+      "https://pub-9db467ada9744f04b4c0eaf2e4b1ca71.r2.dev/video/poster/flesh.png",
+
+    date: "2_20_26",
+
+    description:
+      "flesh Music Video. Directed/Edited by INO. Shot by Tony Lopez. Assisted by Renee Spencer."
+  },
+
+  {
+    title: "From_the_Sticks",
+    featured: false,
+
+    video:
+      "https://pub-9db467ada9744f04b4c0eaf2e4b1ca71.r2.dev/video/fts_MV.mp4",
+
+    poster:
+      "https://pub-9db467ada9744f04b4c0eaf2e4b1ca71.r2.dev/video/poster/FTS.jpg",
+
+    date: "01_14_26",
+
+    description:
+      "FTS Music Video. Directed/Edited by INO. Shot by Tony Lopez"
+  },
+
+  {
+    title: "Star_Gazer",
+    featured: false,
+
+    video:
+      "https://pub-9db467ada9744f04b4c0eaf2e4b1ca71.r2.dev/video/SG_MV.mp4",
+
+    poster:
+      "https://pub-9db467ada9744f04b4c0eaf2e4b1ca71.r2.dev/video/poster/SG.jpg",
+
+    date: "10_24_25",
+
+    description:
+      "SG Music Video. Directed/Edited by INO. Shot by Tony Lopez"
+  }
+];
