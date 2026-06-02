afcbf20ea51a52811ae256f6663a8447930b23e1
INO <musicbyino@gmail.com>
2026-06-02 16:40:22 -0400

    RELEASE.js  AUDIO SECTION OVERHAUL! V1.3

---
+++ releases.js
@@ -0,0 +1,85 @@
+const releases = [
+{
+  title: "swarmin",
+  type: "Single",
+  featured: true,
+
+  cover:
+    "https://pub-9db467ada9744f04b4c0eaf2e4b1ca71.r2.dev/images/audio_card/SWARMING%20COVER.jpg",
+
+  soundcloud:
+    "https://soundcloud.com/musicbyino/swarmin?si=817d004dbfc54efb9e1015ebd4d7cfda&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+
+  spotify:
+    "https://open.spotify.com/track/1hoDIs5f4AwHEOSkYYzqNI?si=70d717b4520c42f0",
+
+  apple:
+    "https://music.apple.com/us/song/swarmin/6770899445",
+
+  youtube:
+    "https://youtu.be/zk3oqOsF_XE?si=YNjvEdRhFRv8-6no"
+},
+
+  {
+    title: "343",
+    type: "Single",
+    featured: false,
+
+    cover:
+      "https://pub-9db467ada9744f04b4c0eaf2e4b1ca71.r2.dev/images/audio_card/343.jpg",
+
+    soundcloud:
+      "https://soundcloud.com/musicbyino/343a1",
+
+    spotify:
+      "https://open.spotify.com/artist/4xRr0YW5BltFbZGEwliT79",
+
+    apple:
+      "https://music.apple.com/us/album/343/1892879329?i=1892879330",
+
+    youtube:
+      "https://youtu.be/APpdhfTtoIw?si=qQfm-jlNxbLjuB-X"
+  },
+
+  {
+    title: "flesh",
+    type: "Single",
+    featured: false,
+
+    cover:
+      "https://pub-9db467ada9744f04b4c0eaf2e4b1ca71.r2.dev/images/audio_card/flesh.png",
+
+    soundcloud:
+      "https://on.soundcloud.com/wFWAipoCEJzmew0HSm",
+
+    spotify:
+      "https://open.spotify.com/track/5fZ8fSePZscDOjmK9Bllnf",
+
+    apple:
+      "https://music.apple.com/us/album/flesh/1892839545?i=1892839547",
+
+    youtube:
+      "https://youtu.be/3dbwldS8Wmo?si=WkHBPxhaYmH-facc"
+  },
+
+  {
+    title: "Funeral Vol. 2",
+    type: "EP",
+    featured: false,
+
+    cover:
+      "https://pub-9db467ada9744f04b4c0eaf2e4b1ca71.r2.dev/images/audio_card/FV2.png",
+
+    soundcloud:
+      "https://on.soundcloud.com/15NL0SaXGE41frIfcD",
+
+    spotify:
+      "https://open.spotify.com/album/41EWun0DCNwJZ2CBd88oGx?si=to2ZGNuyQPuBakRUuYW2gw",
+
+    apple:
+      "https://music.apple.com/us/album/funeral-vol-2-ep/1825076272",
+
+    youtube:
+      ""
+  }
+];
